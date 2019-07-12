d6f4e525b166095cb5ced71217e1184163c8c26c
Walid <hlaliwalid@mail.com>
2019-07-12 11:25:36 +0200

    réecriture du fetch sur l'api JcDecaux -> retour de reponse

--- fetchApiJcd.js
+++ fetchApiJcd.js
@@ -1,34 +1,31 @@
 const rest_api = `https://api.jcdecaux.com/vls/v1/stations?contract=toulouse&apiKey=0765a9c499d7bbb55e963e4f6977e3ac9d3094c4`;
 
 class FetchDataWithButton {
-    constructor(mapSelector, buttonSelector, app_id) {
+    constructor(mapSelector, buttonSelector, app_id, data, stations) {
         this.mapSelector = document.querySelector(`${mapSelector}`);
         this.buttonSelector = document.querySelector(`${buttonSelector}`);
         this.AppId = app_id;
-        this.result1 = [];
-        this.result2 = [];
+        this.data = data;
+        this.stations = stations;
         this.fetchData = function() {
-            const fetchCityName = async (city) => {
-                const api_call = await fetch(`https://api.jcdecaux.com/vls/v1/stations?contract=${city}&apiKey=${this.AppId}`);
-                const data = await api_call.json();
-                return { data };
-            };
-            const showData = () => {
-                fetchCityName(this.mapSelector.value).then((res) => {
-                    this.res = res;
-                    this.result1.state = res.data[0].position.lat;
-                    this.result2.state = res.data[0].position.lng;
-                    console.log(res);
-                    console.log(res.data[0].name);
-                    console.log(this.res);
-                    console.log(this.result1);
-                    console.log(this.result2);
-                });
-            };
-
             this.buttonSelector.addEventListener("click", () => {
-                showData();
+                fetchCityName();
             });
+            const fetchCityName = async () => {
+                const api_call = await fetch(
+                    `https://api.jcdecaux.com/vls/v1/stations?contract=${this.mapSelector.value}&apiKey=${this.AppId}`
+                );
+                this.data = await api_call.json();
+                const filtered = this.data.filter((station) => station);
+                const items = filtered.map((station) => {
+                    stations = { station: station };
+                    console.log(station.position);
+                    return stations;
+                });
+
+                console.log(items);
+                console.log(this.data);
+            };
         };
     }
 }
@@ -37,8 +34,7 @@ const fetchNewCityName = new FetchDataWithButton("#search", ".searchButton", "07
 
 fetchNewCityName.fetchData();
 
-console.log(fetchNewCityName.result1);
-console.log(fetchNewCityName.result2);
+console.log(fetch);
 
 const toulouseMap = new CreateMap(
     "mapid",
---
+++ plan.md
@@ -0,0 +1,12 @@
+```bash
+main.js -->
+fetchApiData.js --> Post Obj Stations
+leafLetMap.js --> Get Obj Stations map Lat and lg on markers
+timer.js --> return Timer
+storeData.js --> validate the form $ execute timer()
+
+
+
+
+
+```
