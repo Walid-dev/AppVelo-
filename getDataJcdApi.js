c28a3d73328bea2d3fc0e4501d920c6893724f3f
Walid <hlaliwalid@mail.com>
2019-07-16 15:41:19 +0200

    popup et markers des stations definis

--- getDataJcdApi.js
+++ getDataJcdApi.js
@@ -1,24 +1,18 @@
 class GetJcdecauxData {
-    constructor(apiKey, cityNameValue, number, hello, data) {
+    constructor(apiKey, cityNameValue, latitude, data, number) {
         this.apiKey = apiKey;
         this.cityNameValue = cityNameValue;
         this.cityNameValue = cityNameInp.value;
-        this.number = number;
         this.data = data;
         this.text = document.querySelector(".text");
-        this.hello = hello;
+        this.latitude = latitude;
+        this.number = number;
         this.getPromise = fetch(
             `https://api.jcdecaux.com/vls/v1/stations?contract=${this.cityNameValue}&apiKey=${this.apiKey}`
         ).then((response) => response.json());
         this.data = this.getPromise.then(
             function(data) {
-                console.log(data);
-                console.log(data[0].number);
-                hello = data[1].name;
-                this.text.textContent = hello;
-                console.log(hello);
-                var proto = data[10].name;
-                console.log(proto);
+                console.log("jcdecaux data", data);
             }.bind(this)
         );
     }
@@ -26,5 +20,3 @@ class GetJcdecauxData {
 
 console.log(cityNameInp.value);
 const cityJcdData = new GetJcdecauxData("0765a9c499d7bbb55e963e4f6977e3ac9d3094c4", "toulouse");
-console.log(cityJcdData.getPromise);
-console.log(cityJcdData.data);
--- leafletMap.js
+++ leafletMap.js
@@ -2,42 +2,42 @@ const jcdPromise = cityJcdData.getPromise;
 const jcdData = cityJcdData.data;
 
 class CreateMap {
-    constructor(mapId, longitude, latitude, zoom, accessToken, marker, mymap, data, hello2) {
+    constructor(mapId, zoom, accessToken, data) {
         this.jcdData = cityJcdData.data;
         this.mapId = mapId;
-        this.longitude = longitude;
-        this.latitude = latitude;
         this.zoom = zoom;
         this.accessToken = accessToken;
-        this.marker = marker;
-        this.mymap = mymap;
         this.promise = jcdPromise;
         this.data = data;
-        this.hello2 = hello2;
         this.text = document.querySelector(".text2");
-
-        this.displayMap = function() {
-            this.mymap = L.map(`${this.mapId}`).setView([this.longitude, this.latitude], this.zoom);
-            marker = L.marker([this.longitude, this.latitude]).addTo(this.mymap);
-            L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-                attribution:
-                    'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-                maxZoom: 18,
-                id: "mapbox.streets",
-                accessToken
-            }).addTo(this.mymap);
-            marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-            console.log("here", this.newdata);
-        };
 
         this.data = this.promise.then(
             function(data) {
-                console.log(data);
-                console.log(data[10].number);
-                hello2 = data[10].name;
-                this.text.textContent = hello2;
-                var proto = data[10].name;
-                console.log(proto);
+                console.log("leafletmap data", data);
+                let dataArray = [""];
+                dataArray.push(data);
+
+                let longitude = data[0].position.lng;
+                let latitude = data[0].position.lat;
+
+                console.log(dataArray[1][1].position);
+
+                const mymap = L.map(`${this.mapId}`).setView([latitude, longitude], this.zoom);
+                console.log(longitude, latitude);
+                const marker = L.marker([latitude, longitude]).addTo(mymap);
+                L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+                    attribution:
+                        'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                    maxZoom: 18,
+                    id: "mapbox.streets",
+                    accessToken
+                }).addTo(mymap);
+                marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+                let i = 0;
+                for (i = 0; i <= data.length; i++) {
+                    const stationsPosition = L.marker([data[i].position.lat, data[i].position.lng]).addTo(mymap);
+                    i++;
+                }
             }.bind(this)
         );
     }
@@ -45,11 +45,6 @@ class CreateMap {
 
 const displayNewMap = new CreateMap(
     "mapid",
-    "43.604462",
-    "1.444247",
-    "13",
+    "11",
     "pk.eyJ1Ijoid2xhZDM0IiwiYSI6ImNqeHA5N25qYTBhZnozbmwzMmdmczBtcGoifQ.hYSWIqrFTCmtKzfE56Y4iw"
 );
-
-displayNewMap.displayMap();
-displayNewMap.getData;
